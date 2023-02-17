e72fa82d7bc9cdf29566508b12822da34c61fd9c
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2023-02-17 13:41:17 +0300

    diğer form elemanları
    Checkbox, slider, radio ve switch widget

---
+++ diger_form_elemanlari/android/app/src/main/kotlin/com/example/diger_form_elemanlari/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diger_form_elemanlari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ diger_form_elemanlari/lib/diger_form_elemanlari.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class DigerFormElemanlari extends StatefulWidget {
+  const DigerFormElemanlari({super.key});
+
+  @override
+  State<DigerFormElemanlari> createState() => _DigerFormElemanlariState();
+}
+
+class _DigerFormElemanlariState extends State<DigerFormElemanlari> {
+  bool? checkBoxState = false;
+  String sehir = "";
+  bool switchState = false;
+  double sliderDeger = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add_circle),
+      ),
+      appBar: AppBar(
+        title: const Text('Diğer Form Elemanları'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: <Widget>[
+            CheckboxListTile(
+              value: checkBoxState,
+              onChanged: (secildi) {
+                setState(() {
+                  checkBoxState = secildi;
+                });
+              },
+              activeColor: Colors.red,
+              title: const Text("Checkbox title"),
+              subtitle: const Text("Checkbox Subtitle"),
+              secondary: const Icon(Icons.add),
+              selected: true,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RadioListTile<String>(
+              value: 'Samsun',
+              groupValue: sehir,
+              onChanged: (deger) {
+                setState(() {
+                  sehir = deger!;
+                  debugPrint("Seçilen değer : $deger");
+                });
+              },
+              title: const Text("Ankara"),
+              subtitle: const Text("Radio Subtitle"),
+              secondary: const Icon(Icons.map),
+            ),
+            RadioListTile<String>(
+              value: 'Osmaniye',
+              groupValue: sehir,
+              onChanged: (deger) {
+                setState(() {
+                  sehir = deger!;
+                  debugPrint("Seçilen değer : $deger");
+                });
+              },
+              title: const Text("Osmaniye"),
+              subtitle: const Text("Radio Subtitle"),
+              secondary: const Icon(Icons.map),
+            ),
+            RadioListTile<String>(
+              value: 'Adana',
+              groupValue: sehir,
+              onChanged: (deger) {
+                setState(() {
+                  sehir = deger!;
+                  debugPrint("Seçilen değer : $deger");
+                });
+              },
+              title: const Text("Adana"),
+              subtitle: const Text("Radio Subtitle"),
+              secondary: const Icon(Icons.map),
+            ),
+            SwitchListTile(
+              value: switchState,
+              onChanged: (deger) {
+                setState(() {
+                  debugPrint("Anlasma onaylandı : $deger");
+                  switchState = deger;
+                });
+              },
+              title: const Text("Switch title"),
+              subtitle: const Text("Switch subtitle"),
+              secondary: const Icon(Icons.refresh),
+            ),
+            const Text("Değeri Sliderden Seçiniz"),
+            Slider(
+              value: sliderDeger,
+              onChanged: (deger) {
+                setState(() {
+                  sliderDeger = deger;
+                });
+              },
+              min: 0,
+              max: 40,
+              divisions: 5,
+              label: sliderDeger.toString(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
